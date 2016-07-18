b871b1003814e71d53b1a186d102a6311fb80480
RubaXa <ibnRubaXa@gmail.com>
2016-07-18 09:13:47 +0300

    * upd

--- demo/ace/xtpl_highlight_rules.js
+++ demo/ace/xtpl_highlight_rules.js
@@ -3,6 +3,38 @@ define('ace/mode/xtpl_highlight_rules', function (require, exports, module) {
 
 	var oop = require('ace/lib/oop');
 	var TextHighlightRules = require('ace/mode/text_highlight_rules').TextHighlightRules;
+	// var R_KEYWORDS = "\\b(if|else|for)\\b";
+	// var R_KEYWORDS_WITH_EXPR = "\\b(if|else|for|in)\\b";
+
+    var R_ESCAPED = "\\\\(?:x[0-9a-fA-F]{2}|" + // hex
+        "u[0-9a-fA-F]{4}|" + // unicode
+        "u{[0-9a-fA-F]{1,6}}|" + // es6 unicode
+        "[0-2][0-7]{0,2}|" + // oct
+        "3[0-7][0-7]?|" + // oct
+        "[4-7][0-7]?|" + //oct
+        ".)";
+
+	function baseTokens() {
+		return [{
+			token : "string",
+			regex : "'(?=.)",
+			next  : "qstring"
+		}, {
+			token : "string",
+			regex : '"(?=.)',
+			next  : "qqstring"
+		}, {
+			token : "constant.numeric", // hex
+			regex : /0(?:[xX][0-9a-fA-F]+|[bB][01]+)\b/
+		}, {
+			token : "constant.numeric", // float
+			regex : /[+-]?\d[\d_]*(?:(?:\.\d*)?(?:[eE][+-]?\d+)?)?\b/
+		},
+		{
+			token: "constant.language.boolean",
+			regex: "true|false"
+		}];
+	}
 
 	function XTPLHighlightRules() {
 		this.$rules = {
@@ -12,23 +44,125 @@ define('ace/mode/xtpl_highlight_rules', function (require, exports, module) {
 					regex: /^\s*\/\/.*$/
 				},
 				{
-					token: "keyword",
-					regex: "#[a-z0-9-_]+"
-				},
-				{
-					token: 'variable',
-					regex: '\\.[a-z0-9-_]+'
-				},
-				{
-					token: 'support.constant',
-					regex: '[a-z0-9-_]+'
-				},
-				{
 					token: 'text',
-					regex: '\|^$'
+					regex: /\|[^$]+/
+				},
+				{
+					token: 'keyword',
+					regex: '\\b(if|for)\\b',
+					next: 'keyword'
+				},
+				{
+					token: 'keyword',
+					regex: '\\belse\\b',
+				},
+				{
+					token: 'keyword',
+					regex: '#[a-z0-9-_]+'
+				},
+				{
+					token: 'entity.other.attribute-name.class',
+					regex: '\\.[a-z_-][a-z0-9-_]+'
+				},
+				{
+					token: ['support.type', 'punctuation.operator'],
+					regex: '([@a-z_-][:a-z0-9-_]*)(:)'
+				},
+				{
+					token: [
+						'paren.lparen',
+						'support.type',
+						'paren.rparen'
+					],
+					regex: '(\\[)([^=]*?)(\\])'
+				},
+				{
+					token: [
+						'paren.lparen',
+						'support.type',
+						'keyword.operator',
+						'string',
+						'paren.rparen'
+					],
+					regex: '(\\[)(.*?)(=)(".*?")(\\])'
+				},
+				{
+					token: 'meta.tag',
+					regex: '[a-z_-][a-z0-9-_]*'
+				},
+				{
+					token : "paren.lparen",
+					regex : "[[({]"
+				}, {
+					token : "paren.rparen",
+					regex : "[\\])}]"
 				},
 				{
 					caseInsensitive: true
+				}
+			].concat(baseTokens()),
+
+			"keyword": baseTokens().concat([
+				{
+					token: 'keyword',
+					regex: 'in',
+				},
+				{
+					regex: '[()]',
+					counter: 0,
+					onMatch: function (chr) {
+						this.next = null;
+
+						if (chr === '(') {
+							this.counter++;
+							return "paren.lparen";
+						} else {
+							this.counter--;
+							
+							if (this.counter === 0) {
+								this.next = "start";
+							}
+
+							return "paren.rparen";
+						}
+					}
+				},
+				{
+					defaultToken: "text"
+				}
+			]),
+
+			"qqstring" : [
+				{
+					token : "constant.language.escape",
+					regex : R_ESCAPED
+				}, {
+					token : "string",
+					regex : "\\\\$",
+					next  : "qqstring"
+				}, {
+					token : "string",
+					regex : '"|$',
+					next  : "start"
+				}, {
+					defaultToken: "string"
+				}
+			],
+			
+			"qstring" : [
+				{
+					token : "constant.language.escape",
+					regex : R_ESCAPED
+				}, {
+					token : "string",
+					regex : "\\\\$",
+					next  : "qstring"
+				}, {
+					token : "string",
+					regex : "'|$",
+					next  : "start"
+				}, {
+					defaultToken: "string"
 				}
 			]
 		};
